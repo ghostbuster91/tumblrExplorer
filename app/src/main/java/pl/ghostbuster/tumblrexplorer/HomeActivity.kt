ec303cca48aafff4ede5b63d15d67de27c05daa3
ghostbuster91 <kghost0@gmail.com>
2016-01-31 17:03:47 +0100

    covert textWatcher to editText extension function

--- app/src/main/java/pl/ghostbuster/tumblrexplorer/HomeActivity.kt
+++ app/src/main/java/pl/ghostbuster/tumblrexplorer/HomeActivity.kt
@@ -3,10 +3,9 @@ package pl.ghostbuster.tumblrexplorer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.widget.EditText
+import pl.ghostbuster.tumblrexplorer.extensions.addOnTextChangedListener
 import pl.ghostbuster.tumblrexplorer.extensions.find
 
 class HomeActivity : AppCompatActivity() {
@@ -17,16 +16,8 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
-        inputEditText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                Log.i("textWatcher", "textChanged")
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
-        })
+        inputEditText.addOnTextChangedListener {
+            Log.i("textWatcher", "textChanged")
+        }
     }
 }
---
+++ app/src/main/java/pl/ghostbuster/tumblrexplorer/extensions/EditTextExtensions.kt
@@ -0,0 +1,19 @@
+package pl.ghostbuster.tumblrexplorer.extensions
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+
+fun EditText.addOnTextChangedListener(listener: (String) -> Unit) {
+    addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(s: Editable) {
+            listener.invoke(s.toString())
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+    })
+}
