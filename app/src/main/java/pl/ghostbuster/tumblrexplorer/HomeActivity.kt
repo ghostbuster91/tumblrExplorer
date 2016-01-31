b13c01efb45f863a321bdbadf13b7efe393f096a
ghostbuster91 <kghost0@gmail.com>
2016-01-31 16:58:45 +0100

    add textWatcher on editText

--- app/src/main/java/pl/ghostbuster/tumblrexplorer/HomeActivity.kt
+++ app/src/main/java/pl/ghostbuster/tumblrexplorer/HomeActivity.kt
@@ -2,15 +2,31 @@ package pl.ghostbuster.tumblrexplorer
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.widget.EditText
 import pl.ghostbuster.tumblrexplorer.extensions.find
 
 class HomeActivity : AppCompatActivity() {
 
     private val inputEditText by find<EditText>(R.id.home_activity_input)
+    private val resultsContainer by find<RecyclerView>(R.id.home_activity_results_container)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
+        inputEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                Log.i("textWatcher", "textChanged")
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
     }
 }
