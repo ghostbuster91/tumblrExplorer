f6f196c5e44799c534e7e6f3040f7a68348424f6
ghostbuster91 <kghost0@gmail.com>
2016-02-01 00:33:20 +0100

    make html links clickable

--- app/src/main/java/pl/ghostbuster/tumblrexplorer/common/extensions/TextViewExtensions.kt
+++ app/src/main/java/pl/ghostbuster/tumblrexplorer/common/extensions/TextViewExtensions.kt
@@ -1,8 +1,10 @@
 package pl.ghostbuster.tumblrexplorer.common.extensions
 
 import android.text.Html
+import android.text.method.LinkMovementMethod
 import android.widget.TextView
 
 fun TextView.setHtml(textToSet: String) {
     text = Html.fromHtml(textToSet)
+    movementMethod = LinkMovementMethod.getInstance()
 }
