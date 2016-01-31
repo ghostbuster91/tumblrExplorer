9526a2ec469b59d7f76bbae56e9bf18f9aa3eec1
ghostbuster91 <kghost0@gmail.com>
2016-01-31 20:01:36 +0100

    expand data model

--- app/src/main/java/pl/ghostbuster/tumblrexplorer/usecase/model/TumblrPost.kt
+++ app/src/main/java/pl/ghostbuster/tumblrexplorer/usecase/model/TumblrPost.kt
@@ -1,3 +1,29 @@
 package pl.ghostbuster.tumblrexplorer.usecase.model
 
-data class TumblrPost(val id: String)
+import com.google.gson.annotations.SerializedName
+
+sealed class TumblrPost(val id: String, val date: String) {
+
+    class QuoteTumblrPost(id: String,
+                          date: String,
+                          val quoteText: String) : TumblrPost(id, date)
+
+    class RegulerTumblrPost(id: String,
+                            date: String,
+                            val regularTitle: String,
+                            val regularBody: String) : TumblrPost(id, date)
+
+    class PhotoTumblrPost(id: String,
+                          date: String,
+                          val photoCaption: String,
+                          @SerializedName("photo-url-100") val photoUrl: String) : TumblrPost(id, date)
+
+    class VideoTumblrPost(id: String,
+                          date: String) : TumblrPost(id, date)
+
+    class LinkTumblrPostid(id: String,
+                           date: String,
+                           val linkText: String,
+                           val linkUrl: String,
+                           val linkDescritpion: String) : TumblrPost(id, date)
+}
