4a378f273e7ac5a637e0456fb8a1631d9673132b
ghostbuster91 <kghost0@gmail.com>
2016-02-02 00:33:22 +0100

    correct hasText extension function

--- app/src/androidTest/java/pl/ghostbuster/tumblrexplorer/functional/extensions/ViewIdExtensions.kt
+++ app/src/androidTest/java/pl/ghostbuster/tumblrexplorer/functional/extensions/ViewIdExtensions.kt
@@ -8,7 +8,7 @@ import android.support.test.espresso.matcher.ViewMatchers.withText
 import pl.ghostbuster.tumblrexplorer.R
 
 infix fun Int.hasText(text: String) {
-    onView(withId(R.id.home_activity_input)).check(matches(withText("")))
+    onView(withId(R.id.home_activity_input)).check(matches(withText(text)))
 }
 
 infix fun Int.typeText(text: String) {
