fb4a9ba3fbdd947bb619a5bfc4d14a682444dbb0
ghostbuster91 <kghost0@gmail.com>
2016-02-02 00:34:28 +0100

    use not hardcoded id in ViewId extension functions

--- app/src/androidTest/java/pl/ghostbuster/tumblrexplorer/functional/extensions/ViewIdExtensions.kt
+++ app/src/androidTest/java/pl/ghostbuster/tumblrexplorer/functional/extensions/ViewIdExtensions.kt
@@ -5,12 +5,11 @@ import android.support.test.espresso.action.ViewActions
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
-import pl.ghostbuster.tumblrexplorer.R
 
 infix fun Int.hasText(text: String) {
-    onView(withId(R.id.home_activity_input)).check(matches(withText(text)))
+    onView(withId(this)).check(matches(withText(text)))
 }
 
 infix fun Int.typeText(text: String) {
-    onView(withId(R.id.home_activity_input)).perform(ViewActions.typeText(text))
+    onView(withId(this)).perform(ViewActions.typeText(text))
 }
